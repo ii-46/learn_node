954bc1e4377d496dbbf665934beb931641f5867c
Inthava XAIYALOUN <coci8750@gmail.com>
2023-01-03 20:38:03 +0700

    firs commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+### Main Resourse
+
+- https://www.youtube.com/playlist?list=PL_c9BZzLwBRKWfmG3DTZ49tDV6Mn7GEQ2
+
+## basic package
+
+this will auto install needed module that are in dependencies (package.json)
+
+```javascript
+npm install
+```
---
+++ src/server.js
@@ -0,0 +1,11 @@
+"use strict";
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello World from server</h1>");
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Server starting..");
+});
